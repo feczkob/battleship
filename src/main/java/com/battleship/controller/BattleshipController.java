8e4385ea8806e2df1c22b96ed937be7a2891e82d
feczkob <feczkob@gmail.com>
2021-11-09 00:24:35 +0100

    removed old repo from controller"

--- src/main/java/com/battleship/controller/BattleshipController.java
+++ src/main/java/com/battleship/controller/BattleshipController.java
@@ -15,7 +15,6 @@ import java.util.List;
 @RestController
 @RequestMapping("/api")
 public class BattleshipController {
-    //private UserRepositoryInMemory userRepositoryInMemory = new UserRepositoryInMemory();
     @Autowired
     private BattleshipService battleshipService;
 
